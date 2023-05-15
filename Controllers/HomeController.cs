39e036bba4d9044dc945d4d24efec311bf0ce1e1
Blessed Sibanda <blasp.net@gmail.com>
2023-05-15 08:54:58 +0200

    Process form data and list positive responses

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers;
 
@@ -8,4 +9,20 @@ public class HomeController : Controller
   {
     return View();
   }
+
+  [HttpGet]
+  public ViewResult RsvpForm() {
+    return View();
+  }
+
+  [HttpPost]
+  public ViewResult RsvpForm(GuestResponse guestResponse) {
+    Repository.AddResponse(guestResponse);
+    return View("Thanks", guestResponse);
+  }
+
+  public ViewResult ListResponses() {
+    return View(Repository.Responses.Where(r => r.WillAttend == true));
+  }
 }
+
